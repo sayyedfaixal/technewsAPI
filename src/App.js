01c6434253a0f0d66a9b237e83673d8bdd884b45
= <=>
2023-03-01 21:32:13 +0530

    App text fixed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <h1>Welcome to Tech News</h1>
+      <p>You can search any Tech news by typing the keyword</p>
       <Search />
       <Pagination />
       <Stories />
