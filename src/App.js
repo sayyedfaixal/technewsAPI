a5242a2eb401a114b9dee1cd2a60b6d03fafd35d
= <=>
2023-03-01 21:36:07 +0530

    App.js heading fixed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <>
       <h1>Welcome to Tech News</h1>
-      <p>You can search any Tech news by typing the keyword</p>
       <Search />
       <Pagination />
       <Stories />
